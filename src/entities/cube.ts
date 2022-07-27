5faa534e4c6f9bff0374fbb5c1b91bfcc3c52dda
mojofill <henryzhang9000@gmail.com>
2022-07-27 18:30:19 +0000

    turned into ts idk why

---
+++ src/entities/cube.ts
@@ -0,0 +1,10 @@
+class CubeObject {
+    width: number;
+    length: number;
+    height: number;
+    constructor(width: number, length: number, height: number) {
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+}
